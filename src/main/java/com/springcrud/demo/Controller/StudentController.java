b6810eedb78f6ded559ae02acfdfbe1f8decb30f
kedar sedai <kedar.sedai05@gmail.com>
2021-04-17 16:30:36 +0545

    Completed Controller part

--- src/main/java/com/springcrud/demo/Controller/StudentController.java
+++ src/main/java/com/springcrud/demo/Controller/StudentController.java
@@ -1,18 +1,56 @@
 package com.springcrud.demo.Controller;
 
+import com.springcrud.demo.Entity.Student;
 import com.springcrud.demo.Service.StudnetServiceImpl;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
+@Controller
 public class StudentController {
 
     @Autowired
     private StudnetServiceImpl studnetService;
 
     @GetMapping("/")
-    private String homePage(Model model) {
-        model.addAttribute("listStudent", studnetService.getAllStudents());
+    public String homePage(Model model) {
+//        model.addAttribute("listStudent", studnetService.getAllStudents());
+//        return "index";
+        List<Student> listStudent = studnetService.getAllStudents();
+        model.addAttribute("listStudent", listStudent);
         return "index";
+    }
+
+    @GetMapping("/new")
+    public String add(Model model) {
+        Student student = new Student();
+//        model.addAttribute("student", new Student());
+        model.addAttribute("student", student);
+        return "newStudnet";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        studnetService.saveStudent(student);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView editStudent(@PathVariable(name = "id") int id) {
+        ModelAndView modelView = new ModelAndView("new");
+        Student student = studnetService.getStudentById(id);
+        modelView.addObject("student", student);
+        return modelView;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable(name = "id") int id) {
+        studnetService.deleteById(id);
+        return "redirect:/";
     }
 }
--- src/main/java/com/springcrud/demo/Service/StudnetServiceImpl.java
+++ src/main/java/com/springcrud/demo/Service/StudnetServiceImpl.java
@@ -3,9 +3,11 @@ package com.springcrud.demo.Service;
 import com.springcrud.demo.Entity.Student;
 import com.springcrud.demo.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class StudnetServiceImpl {
 
     @Autowired
@@ -14,4 +16,17 @@ public class StudnetServiceImpl {
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
+
+    public void saveStudent(Student student) {
+        this.studentRepository.save(student);
+    }
+
+    public Student getStudentById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(int id) {
+        studentRepository.deleteById(id);
+    }
+
 }
